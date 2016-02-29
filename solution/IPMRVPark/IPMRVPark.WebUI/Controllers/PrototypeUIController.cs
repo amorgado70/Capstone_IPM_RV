1d6e455e48289f309d3052ba39cb5e01b9af2957
Alfredo Morgado <alfredo.mor9ado@gmail.com>
2016-02-28 22:20:41 -0500

    New prototypes for Menu, New Reservation, New Customer, New Payment, Login - Responsive for desktops, tablets and phones - From 360px until 760px - Based on Twitter Bootstrap - Uploaded and working on the Web Server : http://www.alvm.com.br/ipmrvpark

--- solution/IPMRVPark/IPMRVPark.WebUI/Controllers/PrototypeUIController.cs
+++ solution/IPMRVPark/IPMRVPark.WebUI/Controllers/PrototypeUIController.cs
@@ -8,6 +8,30 @@ namespace IPMRVPark.WebUI.Controllers
 {
     public class PrototypeUIController : Controller
     {
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult EventMap()
+        {
+            return View();
+        }        
+        public ActionResult Reservation()
+        {
+            return View();
+        }
+        public ActionResult Customer()
+        {
+            return View();
+        }
+        public ActionResult Payment()
+        {
+            return View();
+        }
         public ActionResult ReservationOrderIndex()
         {
             return View();
